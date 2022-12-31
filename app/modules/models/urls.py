bb5f5b049e04ebe15b423da1db82f249a2e1ae04
Konstantin-create <schahov.konst@gmail.com>
2022-12-31 12:09:20 +0300

    feat: db url table class

---
+++ app/modules/models/urls.py
@@ -0,0 +1,12 @@
+from app import db
+from datetime import datetime
+
+
+class Url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String, unique=True)
+    short_url = db.Column(db.String, unique=True)
+    time_stamp = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return f'<Url: {self.short_url} | {self.url}>'
