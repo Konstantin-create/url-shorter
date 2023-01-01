52769c9df6ed2019910dffb059e23c94e991f35a
Konstantin-create <87200874+Konstantin-create@users.noreply.github.com>
2023-01-01 18:16:22 +0300

    feat: check short url api tool

--- app/routes/api/__init__.py
+++ app/routes/api/__init__.py
@@ -11,3 +11,19 @@ def api_create_url():
             return {'short_url': '', 'error': 300}
         return {'short_url': add_url(data['url']).short_url, 'error': 100}
     return {'short_url': '', 'error': 200}
+
+
+@app.route('/api/short/url', methods=['GET', 'POST'])
+def api_check_short_url():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        if 'url' not in data or not data['short_url']:
+            return{'valid': False, 'error': 300}
+        url = get_url_by_short_url(data['short_url']).url
+
+        if url:
+            return {'valid': True, 'error': 100}
+
+        return {'valid': False, 'error': 100}
+    return {'valid': False, 'error': 200}
