308a680eda9f4bb9481055210344f63d9d3bb775
Konstantin-create <87200874+Konstantin-create@users.noreply.github.com>
2023-01-01 17:56:05 +0300

    feat: api create short url handler

--- app/routes/api/__init__.py
+++ app/routes/api/__init__.py
@@ -1,9 +1,13 @@
 from app import app
 from flask import request
+from app.modules.urls_tools import *
 
 
 @app.route('/api/create-url', method=['GET', 'POST'])
 def api_create_url():
     if request.method == 'POST':
-        pass
-    return {'error': 300}
+        data = request.get_json()
+        if 'url' not in data or not data['url']:
+            return {'short_url': '', 'error': 300}
+        return {'short_url': add_url(data['url']).short_url, 'error': 100}
+    return {'short_url': '', 'error': 200}
